b8584da726ce485ec91e5b066680e5b0f2dc5137
Marcin Chojnacki <marcinchojnacki00@gmail.com>
2019-01-20 21:53:06 +0100

    Updates

---
+++ precache-manifest.1746be4d36bca912a7ce6a40e367e4a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/searchcountry-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4d0fad30fa54f5759378",
+    "url": "/searchcountry-app/static/js/runtime~main.4d0fad30.js"
+  },
+  {
+    "revision": "623fc2a389f29e2305a6",
+    "url": "/searchcountry-app/static/js/main.623fc2a3.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/searchcountry-app/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "623fc2a389f29e2305a6",
+    "url": "/searchcountry-app/static/css/main.f161eb0e.chunk.css"
+  },
+  {
+    "revision": "26588c5269329f29cc680347ce0081b9",
+    "url": "/searchcountry-app/index.html"
+  }
+];
